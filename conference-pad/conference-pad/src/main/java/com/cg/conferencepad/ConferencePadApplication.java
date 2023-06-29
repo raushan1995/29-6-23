abfc4d3671c94abe05cacc877ba8304e92f39a8c
Raushan Kumar <priyanshu1996r@gmail.com>
2023-06-29 22:54:24 +0530

    Add files via upload

---
+++ conference-pad/conference-pad/src/main/java/com/cg/conferencepad/ConferencePadApplication.java
@@ -0,0 +1,14 @@
+package com.cg.conferencepad;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferencePadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferencePadApplication.class, args);
+	}
+
+}
